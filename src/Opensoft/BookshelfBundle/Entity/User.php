8c613ca5e7f549cce5d8094c77c8fa758cb2c61d
bstrx <bstrxx@gmail.com>
2015-01-07 12:14:34 -0500

    Translated a bit and some minor changes

--- src/Opensoft/BookshelfBundle/Entity/User.php
+++ src/Opensoft/BookshelfBundle/Entity/User.php
@@ -5,12 +5,13 @@ namespace Opensoft\BookshelfBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="reader")
  */
-class User extends BaseUser
+class User extends BaseUser implements UserInterface
 {
     /**
      * @ORM\Id
